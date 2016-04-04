fde21aaaf0f3be9e3fe4868abeca8b7e2118929d
PuzyreiOleg <puzurey@gmail.com>
2016-04-04 12:15:51 +0300

    Create new calculate

--- calculate.java
+++ calculate.java
@@ -1,5 +1,23 @@
 public class calculate {
 	public static void main(String[] arg){
 		System.out.println("Calculate");
+		
+		float first = Integer.valueOf(arg[0]);
+		float second = Integer.valueOf(arg[1]);
+		
+		float summa = first + second;
+		System.out.println("Summa = " + summa);
+		
+		float minus = first - second;
+		System.out.println("Minus = " + minus);
+		
+		float Umn = first * second;
+		System.out.println("Umnozhit' = " + Umn);
+		
+		if ( second != 0){
+			float Del = first / (float)second;
+			System.out.println("Delenie = " + Del);
+		} else
+			System.out.println("Na 0 delit' nelazia");
 	}
 }
